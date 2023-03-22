d9a3cdd104487dc3c3bf95c6e1d2aa219af0b002
Loïc Cambray <pro.loiccambray@gmail.com>
2023-03-22 15:18:05 +0100

    First Init

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    const WEBSITE_NAME = "Loïc Cambray";
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?=WEBSITE_NAME?> - Accueil</title>
+</head>
+<body>
+    <h1>Site en construction</h1>
+</body>
+</html>
